74a21df30eb122d14ce4dd0c551f8530b1efe804
Frank Schmitt <frankschmitt@gmx.de>
2026-05-10 11:51:31 +0200

    02-09: saturating

--- exercises/02_basic_calculator/09_saturating/src/lib.rs
+++ exercises/02_basic_calculator/09_saturating/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    use std::num::Saturating;
+    let mut result = Saturating(1u32);
     for i in 1..=n {
         // Use saturating multiplication to stop at the maximum value of u32
         // rather than overflowing and wrapping around
         result *= i;
     }
-    result
+    return result.0;
 }
 
 #[cfg(test)]
